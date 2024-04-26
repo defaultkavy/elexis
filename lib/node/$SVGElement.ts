fd4caf0b4a93bf0084c213c015a71afa69b7d9cc
defaultkavy <defaultkavy@gmail.com>
2024-04-26 18:38:32 +0800

    add logo and icons

--- lib/node/$SVGElement.ts
+++
@@ -1,8 +0,0 @@
-import { $Element, $ElementOptions } from "./$Element"
-
-export interface $SVGOptions extends $ElementOptions {}
-export class $SVGElement<S extends SVGElement> extends $Element<S> {
-    constructor(tagname: string, options?: $SVGOptions) {
-        super(tagname, options);
-    }
-}
